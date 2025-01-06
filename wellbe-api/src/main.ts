2fe3f6c2e04c917bbbba1e7aec0cf0e8b01b8588
Andrew Santos <andrew@mayan.com.ph>
2025-01-06 15:45:55 +0800

    UI PORT

--- wellbe-api/src/main.ts
+++ wellbe-api/src/main.ts
@@ -4,6 +4,11 @@ import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.PORTUI, // Allow requests from this origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+    credentials: true, // Allow cookies
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // Automatically strip fields not defined in the DTO
