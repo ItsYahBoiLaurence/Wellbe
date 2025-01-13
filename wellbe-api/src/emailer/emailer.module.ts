e4d6660159775a2b187ee7bd2f06e7f4ca128599
Andrew Santos <andrew@mayan.com.ph>
2025-01-14 07:06:09 +0800

    New API for Employee

--- wellbe-api/src/emailer/emailer.module.ts
+++
@@ -1,8 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { EmailerService } from './emailer.service';
-
-@Global()
-@Module({
-  providers: [EmailerService],
-})
-export class EmailerModule {}
--- wellbe-api/src/engine/dto/generateQuestion.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsDefined, IsEmail, IsNotEmpty } from 'class-validator';
-
-export class GenerateQuestionDTO {
-  @IsEmail({}, { message: 'The email must be a valid email address.' })
-  @IsDefined({ message: 'The "email" property must be defined.' })
-  @IsNotEmpty({ message: 'The "email" property must not be empty.' })
-  email: string;
-}
